4ae3f3206068f233e7e6b346dd8b90f5a27f6290
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-26 18:14:40 +0100

    deserailzie function

--- core/src/main/java/me/bristermitten/mittenlib/config/Configuration.java
+++ core/src/main/java/me/bristermitten/mittenlib/config/Configuration.java
@@ -1,8 +1,14 @@
 package me.bristermitten.mittenlib.config;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.function.Function;
+
 public class Configuration<T> {
     private final String fileName;
     private final Class<T> type;
+    private final @Nullable Function<Map<String, Object>, T> deserializeFunction;
 
     public Configuration(String fileName, Class<T> type) {
         this.fileName = fileName;
