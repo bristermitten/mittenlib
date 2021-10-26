fb67e6d7b4c54f047d547f1321b08f05151b2b12
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-26 13:07:32 +0100

    document StringUtil

--- annotation-processor/src/main/java/me/bristermitten/mittenlib/annotations/util/StringUtil.java
+++ annotation-processor/src/main/java/me/bristermitten/mittenlib/annotations/util/StringUtil.java
@@ -4,6 +4,18 @@ public class StringUtil {
     private StringUtil() {
     }
 
+    /**
+     * Capitalize the first letter of a given String
+     * <ul>
+     * <li>"" => ""</li>
+     * <li>"hello" => "Hello"</li>
+     * <li>"HEllo" => "HEllo"</li>
+     * </ul>
+     *
+     * @param str the String to capitalize
+     * @return If the String is empty, then the string itself (i.e <code>s == capitalize(s)</code>),
+     * otherwise a copy of the string with the first character capitalized
+     */
     public static String capitalize(String str) {
         if (str.isEmpty()) {
             return str;
