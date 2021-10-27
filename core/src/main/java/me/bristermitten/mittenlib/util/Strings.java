142c3d23dbad28b8cc91cf04708c15f3fc4f726d
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-27 11:06:31 +0100

    move StringUtil stuff to Strings

--- core/src/main/java/me/bristermitten/mittenlib/util/Strings.java
+++ core/src/main/java/me/bristermitten/mittenlib/util/Strings.java
@@ -13,6 +13,26 @@ public class Strings {
     }
 
     /**
+     * Capitalize the first letter of a given String
+     * <ul>
+     * <li>"" → ""</li>
+     * <li>"hello" → "Hello"</li>
+     * <li>"HEllo" → "HEllo"</li>
+     * </ul>
+     *
+     * @param str the String to capitalize
+     * @return If the String is empty, then the string itself (i.e <code>s == capitalize(s)</code>),
+     * otherwise a copy of the string with the first character capitalized
+     */
+    public static String capitalize(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        char first = str.charAt(0);
+        return Character.toUpperCase(first) + str.substring(1);
+    }
+
+    /**
      * Join a collection of elements to a String using a given separator and toString function.
      * This is useful when wanting to join a collection of complex objects, whilst wanting to avoid the overhead
      * of Streams ({@link Collectors#joining()}}, and the boilerplate of a for loop
