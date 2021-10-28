bde0ad6de326ba4643554244f4c142d0a49374c0
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-28 17:21:46 +0100

    Make ObjectMapper and implementations more general

--- core/src/main/java/me/bristermitten/mittenlib/config/reader/ObjectMapper.java
+++ core/src/main/java/me/bristermitten/mittenlib/config/reader/ObjectMapper.java
@@ -4,9 +4,31 @@ import me.bristermitten.mittenlib.util.Result;
 
 import java.util.Map;
 
+/**
+ * Interface for mapping objects
+ * It should hold that <code>map(map(t), T.class).getOrThrow().equals(t)</code>, i.e both map functions are inverses
+ * <p>
+ * There are no guarantees made about the Objects taken and returned. An ObjectMapper may use any type it wishes,
+ * as long as the inverses hold true.
+ * 1 ObjectMapper is not required to produce an output compatible with other mappers.
+ */
 public interface ObjectMapper {
-    <T> Result<T> map(Map<String, Object> map, Class<T> type);
+    /**
+     * Map a given object and type to an object of the Type
+     *
+     * @param data The data to map. Usually, this will be a {@link Map}}, but not always
+     * @param type Class of the type to map to
+     * @param <T>  The type to map to
+     * @return A result containing either a mapped object, or an error
+     */
+    <T> Result<T> map(Object data, Class<T> type);
 
-    <T> Map<String, Object> map(T t);
+    /**
+     * Turn a given object into a mapped object
+     *
+     * @param value The type being mapped
+     * @return A mapped representation of the object
+     */
+    Object map(Object value);
 
 }
--- core/src/main/java/me/bristermitten/mittenlib/files/json/GsonObjectMapper.java
+++ core/src/main/java/me/bristermitten/mittenlib/files/json/GsonObjectMapper.java
@@ -3,15 +3,11 @@ package me.bristermitten.mittenlib.files.json;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import me.bristermitten.mittenlib.config.reader.ObjectMapper;
-import me.bristermitten.mittenlib.util.CompositeType;
 import me.bristermitten.mittenlib.util.Result;
 
 import javax.inject.Inject;
-import java.lang.reflect.Type;
-import java.util.Map;
 
 public class GsonObjectMapper implements ObjectMapper {
-    private static final Type MAP_OBJ_OBJ_TYPE = new CompositeType(Map.class, Object.class, Object.class);
     private final Gson gson;
 
     @Inject
@@ -20,16 +16,15 @@ public class GsonObjectMapper implements ObjectMapper {
     }
 
     @Override
-    public <T> Result<T> map(Map<String, Object> map, Class<T> type) {
+    public <T> Result<T> map(Object map, Class<T> type) {
         return Result.runCatching(() -> {
-            final JsonElement tree = gson.toJsonTree(map);
+            final JsonElement tree = map instanceof JsonElement ? (JsonElement) map : gson.toJsonTree(map);
             return gson.fromJson(tree, type);
         });
     }
 
     @Override
-    public <T> Map<String, Object> map(T t) {
-        final JsonElement tree = gson.toJsonTree(t);
-        return gson.fromJson(tree, MAP_OBJ_OBJ_TYPE);
+    public JsonElement map(Object value) {
+        return gson.toJsonTree(value);
     }
 }
