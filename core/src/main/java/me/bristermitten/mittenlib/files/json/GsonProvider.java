256c7313e82137c0a4daa8a89a742f552ced0589
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-28 17:46:05 +0100

    add @Inject -_-

--- core/src/main/java/me/bristermitten/mittenlib/files/json/GsonProvider.java
+++ core/src/main/java/me/bristermitten/mittenlib/files/json/GsonProvider.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapterFactory;
 
+import javax.inject.Inject;
 import javax.inject.Provider;
 import java.util.Set;
 
 public class GsonProvider implements Provider<Gson> {
     private final Set<TypeAdapterFactory> typeAdapterFactories;
 
+    @Inject
     public GsonProvider(Set<TypeAdapterFactory> typeAdapterFactories) {
         this.typeAdapterFactories = typeAdapterFactories;
     }
