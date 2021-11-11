e2b9e6f5e342273aa01b8dd5e0f1af9f9ffb6678
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-11-11 19:01:22 +0000

    upgrade adventure

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     api("com.google.inject:guice:5.0.1")
-    api("net.kyori:adventure-api:4.9.2")
+    api("net.kyori:adventure-api:4.9.3")
     api("net.kyori:adventure-platform-bukkit:4.0.0")
 
 
