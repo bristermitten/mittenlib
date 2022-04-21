13288d4f6a51146cc19f8a1a742cd0dce0d554eb
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-04-21 18:04:53 +0100

    Update adventure & guice

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,9 +3,7 @@ plugins {
 }
 
 dependencies {
-    api("com.google.inject:guice:5.0.1")
-    api("net.kyori:adventure-api:4.9.3")
-    api("net.kyori:adventure-platform-bukkit:4.0.0")
-
-
+    api("com.google.inject:guice:5.1.0")
+    api("net.kyori:adventure-api:4.10.1")
+    api("net.kyori:adventure-platform-bukkit:4.1.0")
 }
