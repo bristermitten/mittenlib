9b804532f4601a9b5fcf345f4246340fc68870ad
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-05-24 21:29:56 +0100

    Make the simple color code hook trigger on every version

--- core/src/main/java/me/bristermitten/mittenlib/lang/format/hook/SimpleColorCodeHook.java
+++ core/src/main/java/me/bristermitten/mittenlib/lang/format/hook/SimpleColorCodeHook.java
@@ -1,6 +1,5 @@
 package me.bristermitten.mittenlib.lang.format.hook;
 
-import me.bristermitten.mittenlib.util.Version;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.Nullable;
@@ -8,7 +7,7 @@ import org.jetbrains.annotations.Nullable;
 public class SimpleColorCodeHook implements FormattingHook {
     @Override
     public boolean shouldRegister() {
-        return Version.getServerVersion().isOlderThan(Version.VER_1_16);
+        return true;
     }
 
     @Override
