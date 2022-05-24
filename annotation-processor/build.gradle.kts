6c5a097b28896c16712ef2af8569ddce33199d0a
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-05-24 21:22:54 +0100

    Fix build error

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -7,8 +7,10 @@ dependencies {
     implementation(project(":core"))
     implementation("com.squareup:javapoet:1.13.0")
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
+    @Suppress("GradlePackageUpdate") // This is deliberately kept low so it syncs with the spigot gson version
+    implementation("com.google.code.gson:gson:2.3.1")
     annotationProcessor("com.google.auto.service:auto-service:1.0.1")
 
-    testImplementation("org.mockito:mockito-core:4.1.0")
+    testImplementation("org.mockito:mockito-core:4.5.1")
     testImplementation("com.google.testing.compile:compile-testing:0.19")
 }
