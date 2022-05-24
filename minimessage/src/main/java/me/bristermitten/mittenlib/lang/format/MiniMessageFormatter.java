a8f40c53487e4652ad2aacf1116aca94b69773d9
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-05-24 18:52:36 +0100

    Fix minimessage module for 1.0.5

--- minimessage/src/main/java/me/bristermitten/mittenlib/lang/format/MiniMessageFormatter.java
+++ minimessage/src/main/java/me/bristermitten/mittenlib/lang/format/MiniMessageFormatter.java
@@ -1,5 +1,6 @@
 package me.bristermitten.mittenlib.lang.format;
 
+import me.bristermitten.mittenlib.collections.Sets;
 import me.bristermitten.mittenlib.lang.format.hook.FormattingHook;
 import net.kyori.adventure.text.Component;
 import org.bukkit.OfflinePlayer;
@@ -7,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class MiniMessageFormatter extends AbstractMessageFormatter {
@@ -22,6 +25,14 @@ public class MiniMessageFormatter extends AbstractMessageFormatter {
     public @NotNull Component format(@NotNull String message, @Nullable OfflinePlayer player) {
         return miniMessageFactory.create(player)
                 .deserialize(preFormat(message, player));
+    }
+
+    @Override
+    public @NotNull MessageFormatter withExtraHooks(@NotNull FormattingHook... hooks) {
+        return new MiniMessageFormatter(
+                Sets.concat(this.hooks, new HashSet<>(Arrays.asList(hooks))),
+                miniMessageFactory
+        );
     }
 
 }
