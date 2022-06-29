a5d8d5cb172ee1d46d798643e6586907414625eb
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-06-29 15:43:16 +0100

    Fix jb dependency i hope

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -9,10 +9,11 @@ dependencies {
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
     @Suppress("GradlePackageUpdate") // This is deliberately kept low so it syncs with the spigot gson version
     implementation("com.google.code.gson:gson:2.3.1")
+
+    implementation("org.jetbrains:annotations:23.0.0")
     annotationProcessor("com.google.auto.service:auto-service:1.0.1")
 
     testImplementation("org.mockito:mockito-core:4.5.1")
     testImplementation("com.google.testing.compile:compile-testing:0.19")
-    testImplementation("org.jetbrains:annotations:23.0.0")
 }
 
