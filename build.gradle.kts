b9e75d0781d0c5e4adaea164ce08f162c192cce1
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-14 20:35:48 +0100

    Bump version to 2.0.0 because I guess we're following semver now

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ subprojects {
 
 
     group = "me.bristermitten"
-    version = "1.0.26-SNAPSHOT"
+    version = "2.0.0-SNAPSHOT"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
