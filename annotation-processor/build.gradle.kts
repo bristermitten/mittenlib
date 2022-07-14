672d1714ddd29b996834da07fd3dbd7baf5b7735
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-14 20:05:37 +0100

    Update mockito

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -7,13 +7,15 @@ dependencies {
     implementation(project(":core"))
     implementation("com.squareup:javapoet:1.13.0")
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
-    @Suppress("GradlePackageUpdate") // This is deliberately kept low so it syncs with the spigot gson version
+    @Suppress("GradlePackageUpdate") // This is deliberately kept low, so it syncs with the spigot gson version
     implementation("com.google.code.gson:gson:2.3.1")
+
+    
 
     implementation("org.jetbrains:annotations:23.0.0")
     annotationProcessor("com.google.auto.service:auto-service:1.0.1")
 
-    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.mockito:mockito-core:4.6.1")
     testImplementation("com.google.testing.compile:compile-testing:0.19")
 }
 
