6f24837bbde2540a0e936aaef93aaf0b1b5a44e8
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-24 19:57:58 +0100

    cursed suppression recursion

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -7,7 +7,10 @@ dependencies {
     implementation(project(":core"))
     implementation("com.squareup:javapoet:1.13.0")
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
-    @Suppress("GradlePackageUpdate") // This is deliberately kept low, so it syncs with the spigot gson version
+    @Suppress(
+        "GradlePackageUpdate",
+        "RedundantSuppression"
+    ) // This is deliberately kept low, so it syncs with the spigot gson version
     implementation("com.google.code.gson:gson:2.3.1")
 
     implementation("com.google.inject:guice:5.1.0")
