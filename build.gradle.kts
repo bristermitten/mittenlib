b1c5bdb25815d20a0783178025ddab808c0d97af
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-25 16:46:05 +0100

    3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ subprojects {
 
 
     group = "me.bristermitten"
-    version = "3.1.0-SNAPSHOT"
+    version = "3.2.0-SNAPSHOT"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
