3a5214ce37d195304bca46e6f9aec12fb5b88136
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-25 16:33:27 +0100

    Improve documentation and add Futures#sequence(Iterable)

--- core/src/main/java/me/bristermitten/mittenlib/util/Futures.java
+++ core/src/main/java/me/bristermitten/mittenlib/util/Futures.java
@@ -1,21 +1,30 @@
 package me.bristermitten.mittenlib.util;
 
+import com.google.common.collect.Iterators;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
+/**
+ * Utility class for working with {@link Future}s and {@link CompletableFuture}s
+ */
 public class Futures {
     private Futures() {
     }
 
     /**
      * Transform an array of {@link CompletableFuture}s into a single {@link CompletableFuture} holding a <code>Collection&lt;T&gt;</code>
-     * This is similar to Haskell's sequence / sequenceA functions, but specialised for CompletableFutures
+     * This is similar to (<a href="https://hackage.haskell.org/package/base-4.16.2.0/docs/GHC-Base.html#v:sequence">Haskell's sequence function</a>),
+     * but specialised for CompletableFutures because we don't have higher kinded types in Java :(
      *
      * @param futures An array of futures
      * @param <T>     The type of the future
-     * @return A single future that will be completed when all of the arguments are, containing all of the argument results in a List
+     * @return A single future that will be completed when all the arguments have completed, containing all the argument results in a List.
+     * The order of the results should be the same as the order of the arguments, but this is not guaranteed.
      */
     @SafeVarargs
     public static <T> CompletableFuture<Collection<T>> sequence(CompletableFuture<T>... futures) {
@@ -33,13 +42,21 @@ public class Futures {
     /**
      * Transform a collection of {@link CompletableFuture}s into a single {@link CompletableFuture} holding a <code>Collection&lt;T&gt;</code>
      *
-     * @param futures An array of futures
-     * @param <T>     The type of the future
-     * @return A single future that will be completed when all of the arguments are, containing all of the argument results in a List
      * @see Futures#sequence(CompletableFuture[])
      */
     public static <T> CompletableFuture<Collection<T>> sequence(Collection<CompletableFuture<T>> futures) {
         //noinspection unchecked
         return sequence(futures.toArray(new CompletableFuture[0]));
+    }
+
+    /**
+     * Transform an {@link Iterable} of {@link CompletableFuture}s into a single {@link CompletableFuture} holding a <code>Collection&lt;T&gt;</code>
+     *
+     * @see Futures#sequence(CompletableFuture[])
+     */
+    public static <T> CompletableFuture<Collection<T>> sequence(Iterable<CompletableFuture<T>> futures) {
+        List<CompletableFuture<T>> futuresList = new LinkedList<>();
+        Iterators.addAll(futuresList, futures.iterator());
+        return sequence(futuresList);
     }
 }
