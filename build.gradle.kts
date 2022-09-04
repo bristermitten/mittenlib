315179769bf79b415dcacac6aa2bec707026aa14
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-09-04 12:33:24 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ subprojects {
 
 
     group = "me.bristermitten"
-    version = "3.8.0-SNAPSHOT"
+    version = "3.9.0-SNAPSHOT"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
