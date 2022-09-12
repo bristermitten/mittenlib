11cada7c863c91fff2897981573ef96d05ef688b
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-09-12 20:02:49 +0100

    bump mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ subprojects {
 
 
     group = "me.bristermitten"
-    version = "3.10.0-SNAPSHOT"
+    version = "3.10.1-SNAPSHOT"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
