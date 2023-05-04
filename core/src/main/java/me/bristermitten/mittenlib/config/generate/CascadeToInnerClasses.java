614e474c01417553151d805b7017553da5467d1a
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-04 23:18:30 +0100

    tidy up doc

--- core/src/main/java/me/bristermitten/mittenlib/config/generate/CascadeToInnerClasses.java
+++ core/src/main/java/me/bristermitten/mittenlib/config/generate/CascadeToInnerClasses.java
@@ -4,8 +4,11 @@ import java.lang.annotation.*;
 
 /**
  * If an Annotation should also apply to inner / nested classes
+ * <p>
  * This is similar to {@link Inherited} but doesn't require a sub/superclass relation,
  * which better suits the patterns often used in config DTO types.
+ * <p>
+ * This is generally for internal use as a marker rather than to change behaviour.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
