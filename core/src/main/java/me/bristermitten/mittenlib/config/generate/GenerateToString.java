0e39903f45adc442429c6fa5f540c016a3c681f1
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-07 21:21:17 +0100

    Revert "tidy up doc"
    This reverts commit 614e474c01417553151d805b7017553da5467d1a.

--- core/src/main/java/me/bristermitten/mittenlib/config/generate/GenerateToString.java
+++ core/src/main/java/me/bristermitten/mittenlib/config/generate/GenerateToString.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 
 /**
  * Instructs the annotation processor to generate a toString method for a generated config class.
- * This behaviour will cascade to inner classes (see {@link CascadeToInnerClasses})
+ * This behaviour will cascade to inner classes (see {@link CascadeToInnerClasses}
  */
 @Target(ElementType.TYPE)
 @Inherited
