23ce2a74ea278f01ee7b74ad861f11382c020f76
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-07-08 12:18:50 +0300

    build(gradle): bump version to 4.2.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ subprojects {
 
 	val libs = rootProject.libs
 	group = "me.bristermitten"
-	version = "4.2.4-SNAPSHOT"
+	version = "4.2.5-SNAPSHOT"
 
 	java {
 		sourceCompatibility = JavaVersion.VERSION_1_8
