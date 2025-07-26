c11a85162b12d30b83a78e470127f1014fd54aff
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-07-26 12:05:03 +0100

    refactor(build): centralise repository definitions

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,13 +47,6 @@ subprojects {
 		}
 	}
 
-	repositories {
-		mavenCentral()
-		maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-		maven("https://oss.sonatype.org/content/repositories/snapshots")
-		maven("https://oss.sonatype.org/content/repositories/central")
-	}
-
 	dependencies {
 		compileOnly(libs.spigot.api)
 
--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -1,11 +1,14 @@
-repositories {
-	maven("https://repo.aikar.co/content/groups/aikar/")
-}
-
 dependencies {
 	implementation(project(":core"))
 	implementation(project(":record-generator:api"))
 	testAnnotationProcessor(project(":record-generator:processor"))
 
 	testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
+	testImplementation(libs.mockbukkit)
+}
+
+
+tasks.compileTestJava {
+	sourceCompatibility = JavaVersion.VERSION_21.toString()
+	targetCompatibility = JavaVersion.VERSION_21.toString()
 }
