66812734e3150d55847937ab4d954e5ed92cf5e0
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-12-20 18:51:23 +0000

    fix SessionID not having equals and hashCode

--- gui/src/main/java/me/bristermitten/mittenlib/gui/session/SessionID.java
+++ gui/src/main/java/me/bristermitten/mittenlib/gui/session/SessionID.java
@@ -11,4 +11,26 @@ public class SessionID<Model, Msg, V extends View<Msg, V, Viewer>, Viewer extend
     public SessionID(UUID id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        SessionID<?, ?, ?, ?> sessionID = (SessionID<?, ?, ?, ?>) obj;
+
+        return id.equals(sessionID.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "SessionID{" +
+                "id=" + id +
+                '}';
+    }
 }
