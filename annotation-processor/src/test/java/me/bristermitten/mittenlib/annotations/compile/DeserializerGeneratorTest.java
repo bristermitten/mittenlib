f3b9c3424a009db3294e5c1576f7e9405f85a2ec
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-06-10 18:03:29 +0100

    test(annotation-processor): fix compilation and execution of existing tests
    Split multiple public classes into separate files in DeserializerGeneratorTest.
    Corrected EnumParsingSchemes imports and assertions in TypesUtilTest to
    ensure they compile properly and pass.

---
+++ annotation-processor/src/test/java/me/bristermitten/mittenlib/annotations/compile/DeserializerGeneratorTest.java
@@ -0,0 +1,101 @@
+package me.bristermitten.mittenlib.annotations.compile;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+import static com.google.testing.compile.Compiler.javac;
+
+import com.google.testing.compile.Compilation;
+import com.google.testing.compile.JavaFileObjects;
+import me.bristermitten.mittenlib.annotations.config.ConfigProcessor;
+import org.junit.jupiter.api.Test;
+
+class DeserializerGeneratorTest {
+
+    @Test
+    void testCompilationWithCustomDeserializersAndEnums() {
+        Compilation compilation = javac().withProcessors(new ConfigProcessor())
+                .compile(
+                        JavaFileObjects.forSourceString("me.bristermitten.mittenlib.tests.CustomType1", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        public class CustomType1 {}
+                        """),
+                        JavaFileObjects.forSourceString("me.bristermitten.mittenlib.tests.CustomType2", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        public class CustomType2 {}
+                        """),
+                        JavaFileObjects.forSourceString("me.bristermitten.mittenlib.tests.TestEnum", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        public enum TestEnum {
+                            ONE, TWO
+                        }
+                        """),
+                        JavaFileObjects.forSourceString(
+                                "me.bristermitten.mittenlib.tests.CustomType1Deserializer", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        import me.bristermitten.mittenlib.config.DeserializationContext;
+                        import me.bristermitten.mittenlib.config.extension.CustomDeserializerFor;
+                        import me.bristermitten.mittenlib.util.Result;
+
+                        @CustomDeserializerFor(CustomType1.class)
+                        public class CustomType1Deserializer {
+                            public static Result<CustomType1> deserialize(DeserializationContext context) {
+                                return Result.ok(new CustomType1());
+                            }
+                        }
+                        """),
+                        JavaFileObjects.forSourceString(
+                                "me.bristermitten.mittenlib.tests.CustomType2Deserializer", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        import me.bristermitten.mittenlib.config.DeserializationContext;
+                        import me.bristermitten.mittenlib.config.extension.CustomDeserializerFor;
+                        import me.bristermitten.mittenlib.config.extension.CustomDeserializer;
+                        import me.bristermitten.mittenlib.util.Result;
+
+                        @CustomDeserializerFor(CustomType2.class)
+                        public class CustomType2Deserializer implements CustomDeserializer<CustomType2> {
+                            @Override
+                            public Result<CustomType2> apply(DeserializationContext context) {
+                                return Result.ok(new CustomType2());
+                            }
+                        }
+                        """),
+                        JavaFileObjects.forSourceString(
+                                "me.bristermitten.mittenlib.tests.DeserializerTestConfigDTO", """
+                        package me.bristermitten.mittenlib.tests;
+
+                        import me.bristermitten.mittenlib.config.Config;
+                        import me.bristermitten.mittenlib.config.EnumParsingScheme;
+                        import me.bristermitten.mittenlib.config.EnumParsingSchemes;
+                        import java.util.List;
+                        import java.util.Map;
+                        import java.util.Set;
+
+                        @Config(requireSerialization = false)
+                        public class DeserializerTestConfigDTO {
+                            public CustomType1 directStaticCustom;
+                            public CustomType2 directNonStaticCustom;
+
+                            public List<CustomType1> listStaticCustom;
+                            public List<CustomType2> listNonStaticCustom;
+
+                            // Nested collections and complex generics
+                            public Map<String, List<CustomType1>> nestedMapWithStaticCustom;
+                            public Set<List<CustomType2>> nestedSetWithNonStaticCustom;
+
+                            @EnumParsingScheme(EnumParsingSchemes.EXACT_MATCH)
+                            public TestEnum exactEnum;
+
+                            @EnumParsingScheme(EnumParsingSchemes.CASE_INSENSITIVE)
+                            public TestEnum lowercaseEnum;
+                        }
+                        """));
+
+        assertThat(compilation).succeeded();
+        assertThat(compilation)
+                .generatedSourceFile("me.bristermitten.mittenlib.tests.DeserializerTestConfigDeserializer");
+    }
+}
