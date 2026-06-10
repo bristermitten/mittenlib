eb207ed33095a93867b052d653a4c8619343230c
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-06-10 12:13:17 +0100

    feat(core): support TYPE_USE target for validation annotations

--- core/src/main/java/me/bristermitten/mittenlib/config/validation/NotBlank.java
+++ core/src/main/java/me/bristermitten/mittenlib/config/validation/NotBlank.java
@@ -5,6 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.METHOD})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.CLASS)
 public @interface NotBlank {}
