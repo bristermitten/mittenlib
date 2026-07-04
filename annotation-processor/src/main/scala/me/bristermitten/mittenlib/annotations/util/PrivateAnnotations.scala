7a39612bf134c71a248c876054db076ba2c80369
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-07-04 18:23:24 +0100

    fix(annotation-processor): manual `@Override` causing issues

--- annotation-processor/src/main/scala/me/bristermitten/mittenlib/annotations/util/PrivateAnnotations.scala
+++ annotation-processor/src/main/scala/me/bristermitten/mittenlib/annotations/util/PrivateAnnotations.scala
@@ -3,7 +3,10 @@ package me.bristermitten.mittenlib.annotations.util
 import me.bristermitten.mittenlib.config.names.ConfigName
 
 object PrivateAnnotations:
-  private val PRIVATE_ANNOTATIONS = Set(classOf[ConfigName].getName)
+  private val PRIVATE_ANNOTATIONS = Set(
+    classOf[ConfigName].getName,
+    classOf[Override].getName
+  )
 
   def isPrivate(annotation: String): Boolean =
     PRIVATE_ANNOTATIONS.contains(annotation)
